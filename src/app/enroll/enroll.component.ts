e55a7e7988be8826289c6bad14ade8c7ef472e42
sudeepthi1022002 <sudeepthireddy102@gmail.com>
2023-05-23 13:58:50 +0530

    first commit

---
+++ src/app/enroll/enroll.component.ts
@@ -0,0 +1,41 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-enroll',
+  templateUrl: './enroll.component.html',
+  styleUrls: ['./enroll.component.css']
+})
+export class EnrollComponent implements OnInit{
+ 
+  
+  
+  constructor(){}
+  ngOnInit(): void{
+    
+  }
+  enrollForm=new FormGroup({
+    firstName : new FormControl(""),
+    lastName: new FormControl(""),
+    age: new FormControl(""),
+    gender: new FormControl(""),
+    fatherName:new FormControl(""),
+    motherName:new FormControl(""),
+    phoneNumber1:new FormControl(""),
+    phoneNumber2:new FormControl(""),
+    emailId: new FormControl(""),
+    houseNo:new FormControl(""),
+    streetName:new FormControl(""),
+    areaName:new FormControl(""),
+    pincode:new FormControl(""),
+    state:new FormControl(""),
+    nationality:new FormControl(""),
+
+  });
+  enrollSubmitted(){
+    console.log(this.enrollForm.value);
+  }
+  }
+
+
